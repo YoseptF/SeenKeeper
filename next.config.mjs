e3b97e6bfb608bba045089444e203ee15de3a682
YoseptF <yosept.flores@gmail.com>
2024-04-17 03:35:01 -0600

    add yarn lock with bun

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,15 @@
 const nextConfig = {
   output: 'export',
   images: {
+    // because of ssg
     unoptimized: true,
   },
+  // to prevent multiple calls to getStaticProps
   reactStrictMode: false,
+  experimental: {
+    // to prevent suspense fallback
+    missingSuspenseWithCSRBailout: false,
+  }
 };
 
 export default nextConfig;
